abe55b39577aeb5d6e0aadece39d9df0f38d65bc
Eone Zhang <eone.zhang@activenetwork.com>
2016-12-15 17:01:19 +0800

    Active Links-Nav Link Wrappers

--- modules/App.js
+++ modules/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router'
+import NavLink from './NavLink'
 
 export default React.createClass({
   render() {
     return <div>
         <h1>React Router Tutorial</h1>
         <ul role="nav">
-          <li><Link to="/about" activeClassName="active">About</Link></li>
-          <li><Link to="/repos" activeClassName="active">Repos</Link></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/repos">Repos</NavLink></li>
         </ul>
         {/* add this */}
         {this.props.children}

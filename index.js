c26db558ca659cc3d545b84accdfd29e2473e6ce
Eone Zhang <eone.zhang@activenetwork.com>
2016-12-15 17:07:30 +0800

    URL Params

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
+import Repo from './modules/Repo'
 
 import { Router, Route, hashHistory } from 'react-router'
 
@@ -11,6 +12,8 @@ render((
     <Route path="/" component={App}>
       {/* make them children of `App` */}
       <Route path="/repos" component={Repos}/>
+      {/* add the new route */}
+      <Route path="/repos/:userName/:repoName" component={Repo}/>
       <Route path="/about" component={About}/>
     </Route>
   </Router>

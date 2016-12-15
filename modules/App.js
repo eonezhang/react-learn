c802223066c082244cc7cad064fdf72f693d6d94
Eone Zhang <eone.zhang@activenetwork.com>
2016-12-15 16:55:26 +0800

    Active Links-Style

--- modules/App.js
+++ modules/App.js
@@ -6,8 +6,8 @@ export default React.createClass({
     return <div>
         <h1>React Router Tutorial</h1>
         <ul role="nav">
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/repos">Repos</Link></li>
+          <li><Link to="/about" activeStyle={{ color: 'red' }}>About</Link></li>
+          <li><Link to="/repos"activeStyle={{ color: 'red' }}>Repos</Link></li>
         </ul>
         {/* add this */}
         {this.props.children}

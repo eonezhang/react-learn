85d7b38d84eebc8c21f05ac06a0f8c94ff698d67
Eone Zhang <eone.zhang@activenetwork.com>
2016-12-15 17:44:41 +0800

    Index Links

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
 import Repo from './modules/Repo'
+import Home from './modules/Home'
 
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
+      <IndexRoute component={Home}/>
       {/* make them children of `App` */}
       <Route path="/repos" component={Repos}>
         <Route path="/repos/:userName/:repoName" component={Repo}/>
